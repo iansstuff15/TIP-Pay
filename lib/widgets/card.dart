b17faba8b4a1b735d7d461d014ff56e15139a701
adrnmlgrto <adrn.mlgrto@gmail.com>
2022-07-13 18:53:05 +0800

    Added details on card

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -3,23 +3,58 @@
 import 'package:flutter/material.dart';
 
 class CardContainer extends StatelessWidget {
- 
-
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 200,
-      padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
+      height: 230,
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
       decoration: BoxDecoration(
-        color: Colors.yellow,
-        
-        borderRadius: BorderRadius.circular(10)
-      ),
+          color: Color.fromARGB(255, 249, 241, 155),
+          borderRadius: BorderRadius.circular(10)),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Card')
+          Text(
+            '1913603',
+            style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Current Balance",
+            style: TextStyle(fontSize: 13),
+          ),
+          Text("₱ 10,000.00",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600)),
+          SizedBox(
+            height: 25,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "Melegrito, John Adrian T.",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "Valid until S.Y. 2024",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
